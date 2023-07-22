ea48c394b86cd5449081c5e6a66458d0ad2e2b2e
Alex Corsega Jr <corsegaalex@gmail.com>
2023-07-23 00:22:22 +0800

    Cloud Database

--- src/Infrastructure/ConfigureServices.cs
+++ src/Infrastructure/ConfigureServices.cs
@@ -25,7 +25,7 @@ public static class ConfigureServices
         else
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(configuration.GetConnectionString("Alex"),
+                options.UseSqlServer(configuration.GetConnectionString("Cloud"),
                     builder => builder.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
         }
 
